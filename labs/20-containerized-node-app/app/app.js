a2b0f34db3ef3c0be2f867e740f2c672db785022
Maéthan <maethan.tordeux@etu.univ-smb.fr>
2026-09-25 08:31:36 +0000

    Enable database queries and quote insertion

--- labs/20-containerized-node-app/app/app.js
+++ labs/20-containerized-node-app/app/app.js
@@ -24,13 +24,11 @@ export async function buildApp() {
   // Get all quotes endpoint
   app.get("/", async (_req, reply) => {
     /******* TODO SELECT quotes from DB ******/
-    // const result = await app.pg.query('SELECT * FROM quotes')
-    // const quotes = result.rows;
+    const result = await app.pg.query('SELECT * FROM quotes')
+    const quotes = result.rows;
+    return reply.view("index.hbs",{quotes});
 
-    // Placeholder test quotes
-    const quotes = [{text: "quote 1", author: "author 1"}, {text: "quote 2", author: "author 2"}]; 
-    return reply.view("index.hbs", { quotes });
-  });
+    });
 
   // Post new quote endpoint
   app.post("/quotes", async (req, reply) => {
@@ -43,7 +41,7 @@ export async function buildApp() {
     }
 
     /******* TODO INSERT quote into DB ******/
-    // await app.pg.query('INSERT INTO quotes (author, text) VALUES ($1, $2)', [author || "anonymous", text]);
+    await app.pg.query('INSERT INTO quotes (author, text) VALUES ($1, $2)', [author || "anonymous", text]);
 
     app.log.info({quote: { author: author || "anonymous", text }}, 'New quote added');
 
